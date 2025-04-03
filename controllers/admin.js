5f4ba601115d0f340a4824a014d740e13e129d89
Zaryab Ahmed Khan <zaryabzak22@gmail.com>
2025-04-03 11:57:31 +0500

    add new routes and controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,3 @@
-// const products = [];
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -22,13 +21,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop/product-list", {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
 };
